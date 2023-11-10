1176b9332c9f32a5bdf8b3c9bfc48085d1f9872f
Alexander Kabatov <57453479+akabatov@users.noreply.github.com>
2023-11-10 15:59:26 -0500

    Minor
    Minor change

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Note import Note
 from scale import Scale
 
 def main():
-    note_c = Note("E", False, False)
+    note_c = Note("B", False, False)
     scale_1 = Scale(note_c, True, False)
     print(scale_1.get_scale())
 
